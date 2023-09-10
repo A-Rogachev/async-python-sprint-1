2e7085cfc6469224e75fc1c94cc57868309ffd66
Aleksandr Rogachev <alxrogachev@gmail.com>
2023-09-10 20:39:54 +0400

    feat: add fetching data task

--- forecasting.py
+++ forecasting.py
@@ -2,7 +2,9 @@
 # import threading
 # import subprocess
 # import multiprocessing
-
+import os
+from pprint import pprint
+from typing import Any
 
 from external.client import YandexWeatherAPI
 from tasks import (
@@ -16,13 +18,19 @@ from utils import CITIES, get_url_by_city_name
 
 def forecast_weather():
     """
-    Анализ погодных условий по городам
+    Анализ погодных условий по городам.
     """
-    # city_name = "MOSCOW"
-    # url_with_data = get_url_by_city_name(city_name)
-    # resp = YandexWeatherAPI.get_forecasting(url_with_data)
-    # print(resp)
-    pass
+
+    data_fetched_task: DataFetchingTask = DataFetchingTask(
+        cities=CITIES,
+        weather_api=YandexWeatherAPI,
+    )
+    fetched_data: tuple[tuple[str, dict[str, Any]]] = data_fetched_task.get_weather_data(
+        max_workers=os.cpu_count() + 3
+    )
+    # print(type(fetched_data))
+
+
 
 
 if __name__ == "__main__":
--- tasks.py
+++ tasks.py
@@ -1,5 +1,52 @@
+from concurrent.futures import ThreadPoolExecutor
+from utils import get_url_by_city_name
+from typing import Any
+
+
 class DataFetchingTask:
-    pass
+    """
+    Получение информации о погодных условиях для указанного списка городов.
+    """
+
+    def __init__(self, cities: dict[str, str], weather_api) -> None:
+        """
+        Инициализация объекта.
+        """
+        self.cities = cities
+        self.weather_api = weather_api
+
+    def get_weather_data(
+        self,
+        max_workers=None
+    ) -> tuple[tuple[str, dict[str, Any] | None]]:
+        """
+        Получение данных.
+        """
+        with ThreadPoolExecutor(max_workers=None) as pool:
+            all_weather_data: set[tuple[str, dict[str, Any]]] = tuple(
+                pool.map(
+                    self.get_weather_data_for_one_city,
+                    self.cities,
+                )
+            )
+            return all_weather_data
+
+    def get_weather_data_for_one_city(
+        self,
+        city: str
+    ) -> tuple[str, dict[str, Any] | None]:
+        """
+        Получение информации о погодных условиях для одного города.
+        """
+        try:
+            weather_data: dict[str, Any] = self.weather_api.get_forecasting(
+                url=get_url_by_city_name(city)
+            )
+        except Exception as error:
+            # здесь сделать логгирование
+            # print(error)
+            return None
+        return city, weather_data
 
 
 class DataCalculationTask:
